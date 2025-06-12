cf9cc30b1054b756b17d722b0c0e17748760ff7e
goli <shirafkanzeynab63@gmail.com>
2025-06-12 08:57:49 +0330

    add basic commands to main

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -9,13 +9,36 @@ import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
-    private static SessionFactory sessionFactory;
 
     public static void main(String[] args) {
-        sessionFactory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .buildSessionFactory();
-        sessionFactory.close();
+        while (true){
+            System.out.println("[L]ogin, [S]ign up: ");
+            String input = scanner.nextLine().toLowerCase();
 
+            if (input.equals("l") || input.equals("login")){
+                System.out.println("Email: ");
+                String email = scanner.nextLine();
+
+                System.out.println("Password: ");
+                String password = scanner.nextLine();
+
+                AuthService.login(email, password);
+            }
+            else if (input.equals("s") || input.equals("sign up")){
+                System.out.println("Name: ");
+                String name = scanner.nextLine();
+
+                System.out.println("Email: ");
+                String email = scanner.nextLine();
+
+                System.out.println("Password: ");
+                String password = scanner.nextLine();
+
+                AuthService.signUp(name, email, password);
+            }
+            else {
+                System.out.println("Invalid option. Please enter [L]ogin or [S]ign up.");
+            }
+        }
     }
 }
