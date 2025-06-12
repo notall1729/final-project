524605a340a3cb6371a9101b19a57d48d2f0d80c
goli <shirafkanzeynab63@gmail.com>
2025-06-12 14:26:23 +0330

    update main

--- src/main/java/aut/ap/Main.java
+++ src/main/java/aut/ap/Main.java
@@ -47,21 +47,35 @@ public class Main {
 
             if (user != null){
                 System.out.println("Welcome back, " + user.getName() + "!");
-                System.out.print("[S]end, [V]iew, [R]eply, [F]orward: ");
+                System.out.println("[S]end, [V]iew, [R]eply, [F]orward: ");
                 String command = scanner.nextLine().toLowerCase();
 
                 if (command.equals("s") || command.equals("send")){
-                    System.out.print("Recipient(s) (comma-separated): ");
+                    System.out.println("Recipient(s) (comma-separated): ");
                     String recipientInput = scanner.nextLine();
                     List<String> recipient = Arrays.asList(recipientInput.split(","));
 
-                    System.out.print("Subject: ");
+                    System.out.println("Subject: ");
                     String subject = scanner.nextLine();
 
-                    System.out.print("Body: ");
+                    System.out.println("Body: ");
                     String body = scanner.nextLine();
 
                     EmailService.sendEmail(user, recipient, subject, body);
+                }
+                else if(command.equals("v") || command.equals("view")){
+                    System.out.println("[A]ll emails, [U]nread emails, [S]ent emails, Read by [C]ode: ");
+                    String newCommand = scanner.nextLine().toLowerCase();
+
+                    if (newCommand.equals("a") || command.equals("u") || command.equals("s")){
+                        EmailService.viewEmails(user, newCommand);
+                    } else if(newCommand.equals("c")){
+                        System.out.println("Code: ");
+                        String emailCode = scanner.nextLine();
+                        EmailService.readEmail(user, emailCode);
+                    } else {
+                        System.out.println("Invalid option.");
+                    }
                 }
             }
         }
